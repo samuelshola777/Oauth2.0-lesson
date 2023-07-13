b8489f7b9a05acf50ff17ac2e75846a9e4db2c63
samuelshola777 <samuelshola14@gmail.com>
2023-07-13 05:08:32 +0100

    first commit

---
+++ Oauth2.0lesson/src/test/java/com/example/Oauth2/lesson/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Oauth2.lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
