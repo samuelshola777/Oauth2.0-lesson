1f9e77963f96e0c33debd509c6040b597698baa8
samuelshola777 <samuelshola14@gmail.com>
2023-07-13 07:42:36 +0100

    filter chain

--- Oauth2.0lesson/src/main/java/com/example/Oauth2/lesson/appConfig/SecurityConfig.java
+++ Oauth2.0lesson/src/main/java/com/example/Oauth2/lesson/appConfig/SecurityConfig.java
@@ -2,17 +2,28 @@ package com.example.Oauth2.lesson.appConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
 @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-    http.
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+            .oauth2Login(withDefaults()).formLogin(withDefaults());
+         //   .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+
+
+    return    http.build();
 
 }
 
